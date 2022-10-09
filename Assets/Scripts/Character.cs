e95491ac7c6732cc851caa100188c9dfefb11ab3
ccsan <cristianc.sanchezr@upb.edu.co>
2022-10-09 14:59:58 -0500

    Scriptable Objects created

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class Character : ScriptableObject
+{
+    public new string name;
+    public int actionPoints;
+    public float health;
+    public float attackPoints;
+    public float defensePoints;
+
+}
